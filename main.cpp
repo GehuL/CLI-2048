a96bc67f833289cda520a6245e67bf1b0b135847
Gehu <lauric.gehu@free.fr>
2023-11-20 10:55:06 +0100

    Bug retiré lors que l'on rentre plusieurs caractères et amélioration de l'affichage

--- main.cpp
+++ main.cpp
@@ -4,6 +4,14 @@
 #include <iostream>
 #include <time.h>
 
+#ifdef _WIN32
+#define CLEAR "CLS"
+#endif
+
+#ifdef __APPLE__
+#define CLEAR "CLEAR"
+#endif
+
 int main(void)
 {
     srand( time( NULL ) );
@@ -11,23 +19,38 @@ int main(void)
     gehul::Board board(5);
     std::cout << board << std::endl;
 
-    char c;
+    bool stop = false;
     do
     {
-        std::cout << "Move (ZQSD): ";
-        std::cin >> c;
-
-        c = toupper(c);
-
-        if(c == 'R')
-            board.restart();
-        else
-            board.move((gehul::Direction) c);
-
-        system("cls");
-
+        system(CLEAR);
         std::cout << "----- 2048 -----\nEnter O to quit. R to restart" << std::endl;
         std::cout << board << std::endl;
+        
+        std::string input;
+        while(input.size() != 1)
+        {
+            std::cout << "Move (ZQSD): ";
+            std::cin >> input;
+            if(input.size() > 1)
+                std::cout << "\nVeuillez entrer un seul caractère" << std::endl;
+        }
 
-    } while(c != 'O');
+        char c = input[0];
+        c = toupper(c);
+
+        switch(c)
+        {
+            case 'R':
+                board.restart();
+                break;
+            case 'O':
+                stop = true;
+                break;
+            default:
+                board.move((gehul::Direction) c);
+                break;
+        }
+  
+
+    } while(!stop);
 }
